90d04ebd31b6ed13df078492c7e4903eb592130e
sahinealam <msahine961@gmail.com>
2026-01-03 12:19:28 +0600

    Post order api creat dan

--- index.js
+++ index.js
@@ -28,19 +28,26 @@ async function run() {
 
     const database = client.db("petservice");
     const petservices = database.collection("services");
+    const orderCollection = database.collection("orders");
     // post api service  add data-base
     app.post("/services", async (req, res) => {
       const data = req.body;
       const dat = new Date();
       data.createdAT = dat;
-      console.log(data);
+      // console.log(data);
 
       const result = await petservices.insertOne(data);
       res.send(result);
     });
     //  get services from Data-Base
     app.get("/services", async (req, res) => {
-      const result = await petservices.find().toArray();
+      const { category } = req.query;
+      // console.log(category);
+      const query = {};
+      if (category) {
+        query.category = category;
+      }
+      const result = await petservices.find(query).toArray();
       res.send(result);
     });
 
@@ -48,7 +55,7 @@ async function run() {
 
     app.get("/services/:id", async (req, res) => {
       const id = req.params;
-      console.log(id);
+      // console.log(id);
       const query = { _id: new ObjectId(id) };
       const result = await petservices.findOne(query);
       res.send(result);
@@ -84,12 +91,20 @@ async function run() {
       res.send(result);
     });
 
+    //  oder post api creat
+    app.post("/orders", async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const result = await orderCollection.insertOne(data);
+      res.status(201).send(result)
+
+      
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
-
     //  all off / commit
     // await client.close();
   }
